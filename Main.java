df1724c072feb6a9d343392b02121dc16e7f8df7
Zaid mushtaq <mushtaqzaid589@ptuniv.edu.in>
2025-02-26 12:23:10 +0530

    main class

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, Java!");
+
+        // Variables and Data Types
+        int number = 10;
+        double pi = 3.14;
+        String name = "Zaid";
+
+        // Print variables
+        System.out.println("Number: " + number);
+        System.out.println("Pi value: " + pi);
+        System.out.println("Hello, " + name + "!");
+
+        // Loop example
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
